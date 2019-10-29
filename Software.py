86a9e449b626477bc6ea83938784df2f973e9379
Adrian1608 <eladrian1608@gmail.com>
2019-10-29 07:33:12 -0500

    El software actualizado prro :v

--- Software.py
+++ Software.py
@@ -9,6 +9,7 @@ productos = ["arrozcosteño", "cocacolapersonal", "inkakolapersonal", "chocolate
 precios = [2.50,2.00,1.50,2.50,3.50,1.50,5.00,3.50,1.20,1.00,2.00,7.00,4.00,3.50,2.50]
 stock = [20,15,15,16,12,25,25,10,14,5,10,15,10,20,50]
 comprados = []
+cantidad_comprados = []
 
 # Cuando el cajero digita los productos:
 
@@ -35,4 +36,15 @@ while True:
         if elbooleano == "no":
             break
 
+while True:
+    for i in range(0,len(comprados)):
+        cantidad = int(input("Ingrese la cantidad del producto " + comprados[i] + " : "))
+        if cantidad > 0:
+            cantidad_comprados.append(cantidad)
+
+    if len(cantidad_comprados) == len(comprados):
+        break
+
+
 print(comprados)
+print(cantidad_comprados)
